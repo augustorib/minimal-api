edc5ddcc083029daef4ef88846f32ad362e1fb4e
augustorib <augustorifra@gmail.com>
2024-10-29 23:25:37 -0300

    feat: post criar veiculos

---
+++ Dominio/DTOs/VeiculoDTO.cs
@@ -0,0 +1,13 @@
+
+namespace minimal_api.Dominio.DTOs
+{
+    public record VeiculoDTO
+    {
+        public string Nome { get; set; } = default!;
+  
+        public string Marca { get; set; } = default!;
+
+        public string Ano { get; set; } = default!;
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,37 +1,60 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using minimal_api.Dominio.DTOs;
+using minimal_api.Dominio.Entidades;
 using minimal_api.Dominio.Interfaces;
 using minimal_api.Dominio.ModelViews;
 using minimal_api.Dominio.Servicos;
 using minimal_api.Infraestrutura.Db;
 
-var builder = WebApplication.CreateBuilder(args);
+#region Builder
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<IAdministradorService, AdministradorService>();
+    builder.Services.AddScoped<IAdministradorService, AdministradorService>();
+    builder.Services.AddScoped<IVeiculosService, VeiculoService>();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<DbContexto>(options => 
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"))
-);
+    builder.Services.AddDbContext<DbContexto>(options => 
+        options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"))
+    );
 
-var app = builder.Build();
+    var app = builder.Build();
+#endregion
 
-app.MapGet("/", () => Results.Json(new Home()));
+#region Home
+    app.MapGet("/", () => Results.Json(new Home()));
+#endregion
 
-app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorService administradorService) => {
-    if(administradorService.Login(loginDTO) != null)
-        return Results.Ok("Login com sucesso!");
-    else
-        return Results.Unauthorized();        
-});
+#region Admniistradores
+    app.MapPost("administradores/login", ([FromBody] LoginDTO loginDTO, IAdministradorService administradorService) => {
+        if(administradorService.Login(loginDTO) != null)
+            return Results.Ok("Login com sucesso!");
+        else
+            return Results.Unauthorized();        
+    });
+#endregion
 
+#region Veiculo
+    app.MapPost("/veiculo", ([FromBody] VeiculoDTO veiculoDTO, IVeiculosService veiculoService) => {
+        
+        var veiculo = new Veiculo{
+            Nome = veiculoDTO.Nome,
+            Marca = veiculoDTO.Marca,
+            Ano = veiculoDTO.Ano
+        };
+        
+        veiculoService.Create(veiculo);
 
-app.UseSwagger();
-app.UseSwaggerUI();
+        return Results.Created($"/veiculo/{veiculo.Id}", veiculo);
+    });
+#endregion
 
-app.Run();
+#region App
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
+    app.Run();
+#endregion
 
